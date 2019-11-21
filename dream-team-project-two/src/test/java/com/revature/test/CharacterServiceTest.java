43afa8a38d896c865470038dfeb7dc692a8c3906
Joseph Lepiscopo <joeycentral@gmail.com>
2019-11-20 20:33:22 -0500

    Implemented copyCharacter, saveCharacter, and getCharactersByUserId method tests in CharacterServiceTest

--- dream-team-project-two/src/test/java/com/revature/test/CharacterServiceTest.java
+++ dream-team-project-two/src/test/java/com/revature/test/CharacterServiceTest.java
@@ -22,23 +22,21 @@ import com.revature.repositories.UserRepository;
 import com.revature.services.CharacterService;
 
 public class CharacterServiceTest {
-		
+
 	@Mock
 	private CharacterRepository mockCharacterRepository;
-	
+
 	@Mock
 	private UserRepository userRepository;
-	
+
 	@InjectMocks
 	private CharacterService characterService;
-	
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
 
-	
 	@Test
 	public void getCharacterByCharacterIdTest() {
 
@@ -46,48 +44,80 @@ public class CharacterServiceTest {
 //		character1.setCharacter_name("Fred");
 //		character1.setId(1);
 //		character1.setPlayername("Critesk");
-		
+
 		Characters character = new Characters();
-		
+
 		int characterId = 1000;
-		
+
 		when(mockCharacterRepository.getCharacterByCharId(characterId)).thenReturn(character);
-		
+
 //		System.out.println("Character ID: " + character.getId());
-		
+
 		Characters returnedCharacter = characterService.getMyCharacter(characterId);
-		
+
 //		System.out.println("returnedCharacter ID: " + returnedCharacter.getId());
-		
+
 		assertSame("Expecting character to be same as returnedCharacter...", character, returnedCharacter);
-		
-		System.out.println("getCharacterByUserId test passed!");
-		
+
+		// System.out.println("getCharacterByUserId test passed!");
 
 	}
-	
+
 	@Test
 	public void getCharactersByUserIdTest() {
 		List<Characters> characters = new ArrayList<>();
-		
+
 		int userId = 50;
-		
+
 		when(mockCharacterRepository.getCharactersByUserId(userId)).thenReturn(characters);
-		
+
 		List<Characters> actualCharacters = characterService.getCharacters(userId);
-		
+
 		assertSame("Expecting a list of two characters to be the same...", characters, actualCharacters);
+
+		// System.out.println("getCharactersByUserId test passed!");
+	}
+
+	@Test
+	public void saveCharacterTest() {
+		Characters character = new Characters();
 		
-		System.out.println("getCharactersByUserId test passed!");
+		character.setId(1);
+		
+		Integer id = 1;
+		
+		when(mockCharacterRepository.save(character)).thenReturn(id);
+		
+		assertSame("Expecting character.getId() to be equivalent to Integer value id...", character.getId(), id);
+		
+	}
+	
+	
+	@Test
+	public void copyCharacterTest() {
+		// Pulls character name, slaps copy, sets ID to 0, then saves new result
+		Characters character = new Characters();
+		
+		character.setId(1);
+		character.setCharacter_name("Dark Magician");
+		
+		Integer copyCharacterId = characterService.copyCharacter(character);
+		
+		System.out.println(copyCharacterId);
+		System.out.println(character.getId());
+		
+		assertTrue("Dark Magician ID is set to 1 and its copy's ID is set to 0", copyCharacterId == 0 && 
+				character.getId() == 0);
+		
 	}
 	
 	@Test
-	public void saveCharacterTest() {
+	public void shareCharacterTest() {
+		// Pulls player name, user object from database that matches player name, replaces player object assoc.
+		// w/ character and sets id to 0, and saves
 		
 		
-		System.out.println("saveCharacter test passed!");
 	}
-	
 	
 
 }
