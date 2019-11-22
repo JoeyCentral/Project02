1a0f8f298ade013c9ac74be83857bc6417b483fd
Joseph Lepiscopo <joeycentral@gmail.com>
2019-11-21 23:40:10 -0500

    Working on the front end with character component.ts. We are now trying to route to the profile page using the character ID that is selected.

--- CharacterGenerator/src/app/components/character/character.component.ts
+++ CharacterGenerator/src/app/components/character/character.component.ts
@@ -4,6 +4,7 @@ import { Character } from 'src/app/models/character';
 import { HttpClient } from '@angular/common/http';
 import { LoginService } from 'src/app/services/login-service.service';
 import { Multiclass } from 'src/app/models/multiclass';
+import { Router } from '@angular/router';
 
 
 
@@ -16,24 +17,45 @@ export class CharacterComponent implements OnInit {
 
   characterList: Character[];
 
-  constructor(private httpClient: HttpClient, private characterService : CharacterService, private loginService : LoginService) { }
-  player=this.loginService.playername;
-  ClassList:Multiclass[];
-  Classes="";
-  
-  
-  
+  constructor(private httpClient: HttpClient, private characterService: CharacterService, private loginService: LoginService,
+    private router: Router) { }
+  player = this.loginService.playername;
+  ClassList: Multiclass[];
+  Classes = "";
+
+
+
   ngOnInit() {
     this.getCharactersHttp();
   }
   async getCharactersHttp() {
-  const url = `http://localhost:8081/character/view/${this.loginService.userId}`;
-  const data = await this.httpClient.get(url).toPromise();
-  
-  console.log(this.loginService.userId);
-  this.characterList=JSON.parse(JSON.stringify(data));
-  const test = this.characterList[1];
-  console.log(test);
-  console.log(test.multiclass)
-};
+    const url = `http://localhost:8081/character/view/${this.loginService.userId}`;
+    const data = await this.httpClient.get(url).toPromise();
+
+    console.log(this.loginService.userId);
+    this.characterList = JSON.parse(JSON.stringify(data));
+    const test = this.characterList[1];
+  };
+
+  async selectCharacter(c : Character) {
+    console.log(c.id);
+    this.characterService.character = await this.characterService.getMyCharacter(c.id)[0];
+    
+          console.log(c);
+          console.log(this.characterService.character)
+          this.router.navigateByUrl("/profile");
+          // Tried to reroute to profile/{char ID}. Able to click on character on character page and retrieve character ID.
+
+
+    // const charChecker = 
+    //   setInterval(()=>{
+    //     if (this.characterService.character) {
+    //       console.log(c);
+    //       console.log(this.characterService.character)
+    //       this.router.navigateByUrl("/profile");
+    //       clearInterval(charChecker);
+    //     }
+    //   }, 100);
+
+  }
 }
